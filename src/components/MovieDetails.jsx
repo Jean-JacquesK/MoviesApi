4fca0d7276118a3971bba32118595f9330140f72
Jean-JacquesK <jj.kechichian@gmail.com>
2023-03-14 10:46:05 +0100

    update gitignore and MovieDetails

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,8 +2,7 @@ function MovieDetails({ title, year, poster }) {
   return (
     <div className='d-flex flex-column align-items-center w-50 p-3 '>
       <h2 className='align-self-center'>
-        {" "}
-        {title} ( {year} ){" "}
+        {title} ( {year} )
       </h2>
       <img className='w-25 ' src={poster} alt={title} />
     </div>
